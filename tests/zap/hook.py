df843837484e3b7feb9bc38f1fa1bffc348ba3c7
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2023-01-11 14:29:52 -0300

    Fix path

--- tests/zap/hook.py
+++ tests/zap/hook.py
@@ -1,5 +1,5 @@
 def zap_started(zap, target):
-   print(zap.script.load('Add Header Script', 'httpsender', 'python : jython', '/zap/wrk/add_header_request.py'))
+   print(zap.script.load('Add Header Script', 'httpsender', 'python : jython', '/zap/wrk/tests/zap/add_header_request.py'))
    print(zap.script.enable('Add Header Script'))
 
 def zap_pre_shutdown(zap):
